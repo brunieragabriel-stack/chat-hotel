f2311d902b3e40d638dee4c209fc75ead163869a
brunieragabriel-stack <brunieragabriel@gmail.com>
2026-06-27 11:45:18 -0300

    feat: adicionando comandos banco de dados

--- app.py
+++ app.py
@@ -5,9 +5,16 @@ from flask_cors import CORS
 from agno.models.openai import OpenAIChat
 from agno.agent import Agent
 from dotenv import load_dotenv
+from supabase import create_client
+import os
 
 #Leitura da chave de API
 load_dotenv()
+supabase_url = os.getenv("SUPABASE_URL")
+#USANDO O GENTEV PARA PEGAR O ARQUIVO ESPECIFICO
+supabase_key = os.getenv("SUPABASE_KEY")
+#criando a conexão com o banco de dados, passand a url e a key
+supabase = create_client(supabase_url,supabase_key)
 #Criar o nosso app
 app = Flask (__name__)
 #Habilitar o cors
@@ -38,6 +45,23 @@ def pergunta():
     resposta = agente.run(pergunta)
     return jsonify({"resposta":resposta.content})
 
+
+#criar a rota para reservas
+@app.route("/reservar", methods=['POST'])
+def reservar():
+    dados = request.get_json()
+    nova_reserva = {
+    "nome":dados["nome"],
+    "email":dados["email"],
+    "check_in":dados["check_in"],
+    "tipo_quarto":dados["tipo_quarto"]
+    }
+    
+    supabase.table("reservas").insert(nova_reserva).execute()
+    return jsonify({"mensagem":"Reserva realizada com sucesso!"})
+
 #Rodar o nosso app
 if __name__ == '__main__':
     app.run(host="0.0.0.0",port=8000)
+
+    
